29a9b8a1172293048d8956e5b35d0fb70b1521ec
Jetug <ksergeev01@gmail.com>
2024-11-17 02:16:39 +0300

    fatman fix

--- src/main/java/com/nukateam/nukacraft/client/models/PowerArmorModel.java
+++ src/main/java/com/nukateam/nukacraft/client/models/PowerArmorModel.java
@@ -1,10 +1,14 @@
 package com.nukateam.nukacraft.client.models;
 
 import com.jetug.chassis_core.client.model.ChassisModel;
+import com.nukateam.ntgl.common.data.util.GunModifierHelper;
 import com.nukateam.ntgl.common.foundation.item.GunItem;
 import com.nukateam.nukacraft.common.foundation.entities.misc.PowerArmorFrame;
 import mod.azure.azurelib.core.animatable.model.CoreGeoBone;
 import mod.azure.azurelib.core.animation.AnimationState;
+import net.minecraft.world.InteractionHand;
+
+import static com.nukateam.ntgl.common.data.util.GunModifierHelper.canRenderInOffhand;
 
 public class PowerArmorModel extends ChassisModel<PowerArmorFrame> {
     public static void setRotations(CoreGeoBone bone, float rot) {
@@ -17,14 +21,28 @@ public class PowerArmorModel extends ChassisModel<PowerArmorFrame> {
     public void setCustomAnimations(PowerArmorFrame animatable, long instanceId, AnimationState<PowerArmorFrame> animationState) {
         super.setCustomAnimations(animatable, instanceId, animationState);
         var animationProcessor = getAnimationProcessor();
+        var rightArm = animationProcessor.getBone("right_arm");
+        var leftArm = animationProcessor.getBone("left_arm");
+        var head = animationProcessor.getBone("head");
 
-        if (animatable.hasPassenger() && animatable.getControllingPassenger().getMainHandItem().getItem() instanceof GunItem gunItem) {
-            gunItem.getGun().getGeneral().getGripType().getHeldAnimation()
-                    .applyGeoModelRotation(animatable, animationProcessor);
+        if (animatable.hasPassenger()){
+            var mainHandItem = animatable.getControllingPassenger().getMainHandItem();
+            var offHandItem = animatable.getControllingPassenger().getOffhandItem();
+
+            if(mainHandItem.getItem() instanceof GunItem){
+                GunModifierHelper
+                        .getGripType(mainHandItem)
+                        .getHeldAnimation()
+                        .applyGeoModelRotation(animatable, rightArm, leftArm, head, InteractionHand.MAIN_HAND);
+            }
+
+            if(offHandItem.getItem() instanceof GunItem && canRenderInOffhand(offHandItem) && canRenderInOffhand(mainHandItem)) {
+                GunModifierHelper
+                        .getGripType(mainHandItem)
+                        .getHeldAnimation()
+                        .applyGeoModelRotation(animatable, rightArm, leftArm, head, InteractionHand.OFF_HAND);
+            }
         } else if (animatable.armsAnimation == null) {
-            var rightArm = animationProcessor.getBone("right_arm");
-            var leftArm = animationProcessor.getBone("left_arm");
-
             setRotations(rightArm, 0);
             setRotations(leftArm, 0);
         }
